fce9cb49e78b87a0b40873145fdecde7ca3ae3b5
vincent-tarrit <91567748+vincent-tarrit@users.noreply.github.com>
2022-11-08 17:25:17 +0100

    Update kgit.rb

--- kgit.rb
+++ kgit.rb
@@ -3,7 +3,7 @@ class Kgit < Formula
   homepage "https://github.com/vincent-tarrit/kGit"
   url "https://github.com/vincent-tarrit/kGit/blob/main/build/kgit.phar?raw=true&date=2022-11-08-17-24"
   version "0.0.5"
-  sha256 "283c032e1d9b9e7c4f504d99fbbc7f84b59e12ea45d77c01052fb57d34b4fa70"
+  sha256 "206da5a1c0658e9d5f37aa9f92862553d749b645798634e28e7e51fa09aef321"
   license "MIT"
 
   def install
