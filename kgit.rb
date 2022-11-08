658f52b0684a70ce66398698e4488e60b8ff00ff
vincent-tarrit <91567748+vincent-tarrit@users.noreply.github.com>
2022-11-08 17:29:54 +0100

    Update kgit.rb

--- kgit.rb
+++ kgit.rb
@@ -1,13 +1,13 @@
 class Kgit < Formula
   desc "This package allow you to create and deploy easily on Infomaniak"
   homepage "https://github.com/vincent-tarrit/kGit"
-  url "https://github.com/vincent-tarrit/kGit/blob/main/build/kgit.phar?raw=true&date=2022-11-08-17-26"
+  url "https://github.com/vincent-tarrit/kGit/blob/main/build/kgit-v0.0.6.phar?raw=true"
   version "0.0.6"
   sha256 "206da5a1c0658e9d5f37aa9f92862553d749b645798634e28e7e51fa09aef321"
   license "MIT"
 
   def install
-    bin.install "kgit.phar" => "kgit"
+    bin.install "kgit-v.0.0.6.phar" => "kgit"
   end
   test do
     system "false"
