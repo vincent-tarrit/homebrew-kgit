c8bedd66bae790e16a29acb18569ba9533a501cb
Vincent <vincent.tarrit29@gmail.com>
2022-11-09 16:34:06 +0100

    sha v0.0.2

--- kgit.rb
+++ kgit.rb
@@ -3,7 +3,7 @@ class Kgit < Formula
   homepage "https://github.com/vincent-tarrit/kGit"
   url "https://github.com/vincent-tarrit/kGit/blob/main/build/kgit-v0.0.2.phar?raw=true"
   version "0.0.2"
-  sha256 "f80c710227911e9d027e65f852b0806fad2ab931f4c2b0af94e894efc580a841"
+  sha256 "2136add9e0bf10c9f0a075cb0f717bc5297a560dc969b1bb39d1a9013b58c782"
   license "MIT"
 
   def install
